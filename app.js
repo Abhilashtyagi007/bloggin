5f7187f8ccd73d1b410174fd8707b52436be96af
tyagi <abhilash.bhumca19@gmail.com>
2021-06-19 22:24:15 +0530

    completed project

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+
+const { urlencoded } = require('express');
+
+const blogRoutes = require('./routes/blogRoutes');
+
+
+
+// express app
+const app = express();
+
+// connect to mangoDB
+const dbURI = 'mongodb+srv://Tyagi:tyagityagi@nodetute.1evii.mongodb.net/nodeblog-db?retryWrite s=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true , useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+ 
+
+// register view engine
+app.set('view engine','ejs');
+
+// listen for request
+//app.listen(3000);
+
+// middleware & static 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+
+app.get('/',(req,res) =>{
+    res.redirect('/blogs');
+})
+
+app.get('/about', (req,res) => {
+   res.render('about',{title: 'About'});
+})
+
+// blog routes
+app.use('/blogs',blogRoutes);
+
+app.use((req,res) =>{
+    res.status(404).render('404',{title: '404'});
+})
